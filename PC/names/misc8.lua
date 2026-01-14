64d10d8d29145e06e5a97780ad41b3acbfc70268
Sam Gray <samleegray@gmail.com>
2026-01-13 20:45:19 -0800

    More names for my guild mates.

--- PC/names/misc8.lua
+++ PC/names/misc8.lua
@@ -7,3 +7,6 @@ local n = lib.GetNamesTable()
 
 n["@RedBoul"] = {"RedBoul", "|ca8211eR|r|cb7422fe|r|cc56240d|r|cd48252B|r|ce2a263o|r|cf1c374u|r|cffe385l|r"}
 n["@samihaize"] = {"samihaize", "|cf500e2s|r|ceb00e5a|r|ce100e9m|r|cd700edi|r|cce00f0h|r|cc400f4a|r|cba00f8i|r|cb000fbz|r|ca600ffe|r"}
+n["@ikillmeee"] = {"ikillmeee", "|c23ff00i|r|c21ff00k|r|c1fff00i|r|c1dff00l|r|c1bff00l|r|c19ff00m|r|c17ff00e|r|c15ff00e|r|c13ff00e|r"}
+n["@CodieRay"] = {"CodieRay", "|ce1c841C|r|cc4d05co|r|ca6d777d|r|c88de93i|r|c6ae5aee|r|c4dedc9R|r|c2ff4e4a|r|c11fbffy|r"}
+n["@DejaEntendude"] = {"DejaEntendude", "|cd314ffD|r|cd315ffe|r|cd315ffj|r|cd216ffa|r|cd216ffE|r|cd216ffn|r|cd217fft|r|cd217ffe|r|cd217ffn|r|cd118ffd|r|cd118ffu|r|cd119ffd|r|cd119ffe|r"}
